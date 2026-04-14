f23780a48290808373b2df5d154e72b9c6a5eda8
Lajos Meszaros <m_lajos@hotmail.com>
2026-04-14 18:22:02 +0200

    feat: add uv coordinates to polygons

--- src/index.ts
+++ src/index.ts
@@ -15,9 +15,9 @@ import {
   MeshLambertMaterial,
   PerspectiveCamera,
   PointLight,
-  // PointLightHelper,
   Scene,
   Timer,
+  Vector2,
   Vector3,
   WebGLRenderer,
   WireframeGeometry,
@@ -334,6 +334,7 @@ function createMesh(
 ): Mesh {
   const vertices: number[] = []
   const normals: number[] = []
+  const uvs: number[] = []
 
   fts.polygons.forEach((polygonData) => {
     if (filter(polygonData) === false) {
@@ -341,10 +342,8 @@ function createMesh(
     }
 
     if (isQuad(polygonData)) {
-      const [a, b, c, d] = polygonData.vertices.map((vertex) => {
-        return arxVector3toVector3(vertex)
-          .sub(offset)
-          .multiply(new Vector3(-1, -1, 1))
+      const [a, b, c, d] = polygonData.vertices.map(({ x, y, z }) => {
+        return new Vector3(x, y, z).sub(offset).multiply(new Vector3(-1, -1, 1))
       })
 
       // prettier-ignore
@@ -360,8 +359,8 @@ function createMesh(
 
       const [nA, nB, nC, nD] = (
         polygonData.normals ?? [polygonData.norm, polygonData.norm, polygonData.norm, polygonData.norm2]
-      ).map((vector) => {
-        return arxVector3toVector3(vector).multiply(new Vector3(-1, -1, 1))
+      ).map(({ x, y, z }) => {
+        return new Vector3(x, y, z).multiply(new Vector3(-1, -1, 1))
       })
 
       // prettier-ignore
@@ -374,11 +373,24 @@ function createMesh(
         ...nB.toArray(),
         ...nD.toArray(),
       )
+
+      const [uvA, uvB, uvC, uvD] = polygonData.vertices.map(({ u, v }) => {
+        return new Vector2(u, v)
+      })
+
+      // prettier-ignore
+      uvs.push(
+        ...uvA.toArray(),
+        ...uvB.toArray(),
+        ...uvC.toArray(),
+
+        ...uvC.toArray(),
+        ...uvB.toArray(),
+        ...uvD.toArray(),
+      )
     } else {
-      const [a, b, c] = polygonData.vertices.map((vertex) => {
-        return arxVector3toVector3(vertex)
-          .sub(offset)
-          .multiply(new Vector3(-1, -1, 1))
+      const [a, b, c] = polygonData.vertices.map(({ x, y, z }) => {
+        return new Vector3(x, y, z).sub(offset).multiply(new Vector3(-1, -1, 1))
       })
 
       // prettier-ignore
@@ -389,8 +401,8 @@ function createMesh(
       )
 
       const [nA, nB, nC] = (polygonData.normals ?? [polygonData.norm, polygonData.norm, polygonData.norm]).map(
-        (vector) => {
-          return arxVector3toVector3(vector).multiply(new Vector3(-1, -1, 1))
+        ({ x, y, z }) => {
+          return new Vector3(x, y, z).multiply(new Vector3(-1, -1, 1))
         },
       )
 
@@ -400,12 +412,24 @@ function createMesh(
         ...nB.toArray(),
         ...nC.toArray(),
       )
+
+      const [uvA, uvB, uvC] = polygonData.vertices.map(({ u, v }) => {
+        return new Vector2(u, v)
+      })
+
+      // prettier-ignore
+      uvs.push(
+        ...uvA.toArray(),
+        ...uvB.toArray(),
+        ...uvC.toArray(),
+      )
     }
   })
 
   const geometry = new BufferGeometry()
   geometry.setAttribute('position', new BufferAttribute(new Float32Array(vertices), 3))
   geometry.setAttribute('normal', new BufferAttribute(new Float32Array(normals), 3))
+  geometry.setAttribute('uv', new BufferAttribute(new Float32Array(uvs), 2))
 
   return new Mesh(geometry, material)
 }
@@ -630,9 +654,6 @@ for (const light of llf.lights) {
   pointLight.position.set(-light.pos.x, -light.pos.y, light.pos.z)
 
   scene.add(pointLight)
-
-  // const helper = new PointLightHelper(pointLight, 10)
-  // scene.add(helper)
 }
 
 // TODO: make lights toggleable

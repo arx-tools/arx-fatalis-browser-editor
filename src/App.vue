8198cf5c2d00d3465f4b83641b872582ce735fca
Lajos Meszaros <m_lajos@hotmail.com>
2026-05-30 23:41:12 +0200

    refactor: create Header component

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,8 @@
 <script setup lang="ts">
+import Header from './components/Header.vue'
 import { handleDownloadButtonClick } from './main'
-import { uiTitle, isLoading } from './ui/ui'
 </script>
 
 <template>
-  <header>
-    <h2>{{ uiTitle }}</h2>
-    <button :disabled="isLoading !== 'fulfilled'" @click="handleDownloadButtonClick">
-      <i class="fa-solid fa-download"></i>
-      Download
-    </button>
-  </header>
+  <Header @download="handleDownloadButtonClick" />
 </template>
-
-<style lang="scss" scoped>
-header {
-  display: flex;
-  justify-content: space-between;
-  padding: 10px;
-  background: #2c2828;
-  color: #d3c0c0;
-
-  h2 {
-    margin: 0;
-  }
-}
-</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+import { uiTitle, isLoading } from '../ui/ui'
+
+defineEmits<{
+  (event: 'download'): void
+}>()
+</script>
+
+<template>
+  <header>
+    <h2>{{ uiTitle }}</h2>
+    <button :disabled="isLoading !== 'fulfilled'" @click="$emit('download')">
+      <i class="fa-solid fa-download"></i>
+      Download
+    </button>
+  </header>
+</template>
+
+<style lang="scss" scoped>
+header {
+  display: flex;
+  justify-content: space-between;
+  padding: 10px;
+  background: #2c2828;
+  color: #d3c0c0;
+
+  h2 {
+    margin: 0;
+  }
+}
+</style>
